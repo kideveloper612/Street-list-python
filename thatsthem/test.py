eefcca1bd6067b5d87b40b7f8e2f77b558099935
kideveloper612 <developer61510@gmail.com>
2020-06-01 11:32:13 +0800

    test

--- thatsthem/test.py
+++ thatsthem/test.py
@@ -1,11 +1,11 @@
 print("# Create new threads")
-for i in range(252):
-    print("thread{} = myThread({}, thread_number_list[{}], proxy_list[{}])".format(i+1, i+1, i, i))
+for i in range(500):
+    print("thread{} = myThread({})".format(i+1, i+1, i, i))
 print("# Start new Threads")
-for i in range(252):
+for i in range(500):
     print("thread{}.start()".format(i+1))
 
 print("# Add threads to thread list")
-for i in range(252):
+for i in range(500):
     print("threads.append(thread{})".format(i+1))
 
